5c08862822cc87394af04f0615a7e6392dac4adc
Ivan Vavilov <ivan.vavilov@verkada.com>
2026-05-27 11:07:04 -0700

    Update 0.0.1 release checksum

--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
         .binaryTarget(
             name: "VerkadaPassSDK",
             url: "https://github.com/verkada/Verkada-Pass-iOS-SDK/releases/download/0.0.1/VerkadaPassSDK.xcframework.zip",
-            checksum: "5d64daab3a650f80c95a1fef8e316caaa5dae7bfb8b4801c3f86b340dbb16644"
+            checksum: "623194282095ecd2da5ab56f2ab9b469dbdc1252c0031789c96b879f02898bba"
         ),
     ]
 )

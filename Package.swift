f5c87d63928cfd3cb25e2b577032cbacf35e571f
Ivan Vavilov <ivan.vavilov@verkada.com>
2026-05-27 13:27:32 -0700

    Add wrapper

--- Package.swift
+++ Package.swift
@@ -9,10 +9,24 @@ let package = Package(
     products: [
         .library(
             name: "VerkadaPassSDK",
-            targets: ["VerkadaPassSDK"]
+            targets: ["VerkadaPassSDKWrapper"]
         ),
     ],
+    dependencies: [
+        .package(url: "https://github.com/kishikawakatsumi/KeychainAccess.git", from: "4.2.0"),
+        .package(url: "https://github.com/jedisct1/swift-sodium.git",
+                 revision: "cfd195c76882aa9b997560ca7cb95d72fbf5db00"),
+    ],
     targets: [
+        .target(
+            name: "VerkadaPassSDKWrapper",
+            dependencies: [
+                .target(name: "VerkadaPassSDK"),
+                .product(name: "KeychainAccess", package: "KeychainAccess"),
+                .product(name: "Sodium", package: "swift-sodium"),
+            ],
+            path: "Sources/VerkadaPassSDKWrapper"
+        ),
         .binaryTarget(
             name: "VerkadaPassSDK",
             url: "https://github.com/verkada/Verkada-Pass-iOS-SDK/releases/download/0.1/VerkadaPassSDK.xcframework.zip",
